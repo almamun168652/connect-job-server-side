26ce92847704134df3e0c2c4ad4b534d60ee779d
Al Mamun <wdmamun75@gmail.com>
2023-11-09 15:32:44 +0600

    added one space

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 // app.use(cookieParser());
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ywavnlw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
